9fb91e7c8574d4b0dc33397e15895ff10115cce1
fellipessanha <fellipe900@gmail.com>
2024-09-10 19:11:57 -0300

    fix: set correct index in group by year

--- src/historical_mean.py
+++ src/historical_mean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def group_data_by_day_of_year(df: pd.DataFrame, idx):
+def group_data_by_day_of_year(df: pd.DataFrame, idx) -> pd.DataFrame:
     historical_data = df.copy()
     historical_data["doy"] = historical_data.index.day_of_year
     historical_data[idx] = historical_data.index
@@ -10,4 +10,4 @@ def group_data_by_day_of_year(df: pd.DataFrame, idx):
     historical_mean[idx] = historical_mean[idx].apply(
         lambda d: d.replace(hour=0, minute=0, second=0, microsecond=0, nanosecond=0)
     )
-    return historical_mean.set_index(idx)
+    return historical_mean.set_index("doy")
